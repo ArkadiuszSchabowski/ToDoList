1a23c64f894e797fc4eb2177375dcf787e372682
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-11-05 23:41:45 +0100

    fix: make description non-nullable

--- ToDoList_Server/Models/AddTaskDto.cs
+++ ToDoList_Server/Models/AddTaskDto.cs
@@ -8,6 +8,7 @@ namespace ToDoList_Server.Models
         [StringLength(50, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 50 characters.")]
         public string Title { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Description is required.")]
         [StringLength(200, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 200 characters.")]
         public string? Description { get; set; }
 
