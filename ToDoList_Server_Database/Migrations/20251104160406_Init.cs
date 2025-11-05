ae45efda40a0e098b2664a1015e229852a18efae
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-11-05 01:22:07 +0100

    feat: create database of type MSSQL (temporary)

---
+++ ToDoList_Server_Database/Migrations/20251104160406_Init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList_Server_Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskItems");
+        }
+    }
+}
---
+++ ToDoList_Server_Database/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using ToDoList_Server_Database.Entities;
+
+namespace ToDoList_Server_Database
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<TaskItem> TaskItems { get; set; }
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
