a5e163a133301def3287bdc01f70aa86ffb0168e
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-11-07 23:33:55 +0100

    fix: update tests

--- Client/ToDoList_Client/src/app/components/public/home/home.component.spec.ts
+++ Client/ToDoList_Client/src/app/components/public/home/home.component.spec.ts
@@ -3,18 +3,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let toastrService: ToastrService;
+
+  class MockToastrService {}
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BrowserAnimationsModule, HomeComponent, HttpClientTestingModule],
+      imports: [
+        BrowserAnimationsModule,
+        HomeComponent,
+        HttpClientTestingModule,
+        ToastrModule,
+      ],
+      providers: [{ provide: ToastrService, useClass: MockToastrService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
     fixture.detectChanges();
   });
 
--- Client/ToDoList_Client/src/app/components/public/task-form/task-form.component.spec.ts
+++ Client/ToDoList_Client/src/app/components/public/task-form/task-form.component.spec.ts
@@ -3,19 +3,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TaskFormComponent } from './task-form.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 describe('TaskFormComponent', () => {
   let component: TaskFormComponent;
   let fixture: ComponentFixture<TaskFormComponent>;
+  let toastrService: ToastrService;
+
+  class MockToastrService{}
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BrowserAnimationsModule, HttpClientTestingModule, TaskFormComponent]
-    })
-    .compileComponents();
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        TaskFormComponent,
+        ToastrModule,
+      ],
+      providers: [{ provide: ToastrService, useClass: MockToastrService }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TaskFormComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
     fixture.detectChanges();
   });
 
